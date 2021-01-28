03873b914585aada7cbab8f61d9ab273ca65772a
Evgenii T <chasej18@inbox.ru>
2021-01-28 18:43:38 +0300

    fix logging

--- page_loader/logging.py
+++ page_loader/logging.py
@@ -1,6 +1,7 @@
 # _*_ coding: utf-8 _*_
 import logging
+import sys
 
 
 def set_logging():
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')  # noqa: E501
+    logging.basicConfig(level=logging.INFO, stream=sys.stderr, format='%(levelname)s: %(message)s')  # noqa: E501
