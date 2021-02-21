460b2c1523ce6e60a3ef4cc20466325fc04f6620
Evgenii T <chasej18@inbox.ru>
2021-02-21 23:41:38 +0300

    Refactoring again

--- tests/test_files_name.py
+++ tests/test_files_name.py
@@ -3,24 +3,11 @@ import pytest
 from page_loader.url import to_file_name
 
 
-cases = [
-    (
-     'https://ru.hexlet.io/courses',
-     'ru-hexlet-io-courses.html'
-    ),
-    (
-     'https://www.google.ru',
-     'www-google-ru.html'
-    ),
-    (
-     'https://klavogonki.ru/gamelist',
-     'klavogonki-ru-gamelist.html'
-    )
-]
+cases = [('https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.html'),
+         ('https://www.google.ru', 'www-google-ru.html'),
+         ('https://klavogonki.ru/gamelist', 'klavogonki-ru-gamelist.html')]
 
 
 @pytest.mark.parametrize('url, expected', cases)
 def test_get_files_name(url, expected):
     assert to_file_name(url) == expected
-
-
